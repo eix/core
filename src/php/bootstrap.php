f2917b06e2b5f961548e17b5ac7381eb9edb15bb
Max Noé <max@nohex.com>
2013-04-06 02:40:35 +0200

    Class-loader overhaul.
    An improved file location method has allowed returning the class loader to its original implementation, base on the include path.

--- src/php/bootstrap.php
+++ src/php/bootstrap.php
@@ -1,13 +1,30 @@
 <?php
 /**
- * This script registers Eix's autoloader.
+ * This is Eix's standard bootstrap, which adds the main/, lib/ and test/
+ * folders of the current library/phar/application into the include path, and
+ * makes sure the class loader is initialised.
  */
 
-require_once __DIR__ . '/main/Nohex/Eix/Core/ClassLoader.php';
-
-// Classes will be loaded from the main/ folder.
-\Nohex\Eix\Core\ClassLoader::addClassPath(__DIR__ . '/main/');
-// If available, classes will be also loaded from the lib/ folder.
-\Nohex\Eix\Core\ClassLoader::addClassPath(__DIR__ . '/lib/', false);
-// If available, classes will be also loaded from the test/ folder.
-\Nohex\Eix\Core\ClassLoader::addClassPath(__DIR__ . '/test/', false);
+// Get the class loader.
+$classLoader = null;
+if (class_exists('\\Nohex\\Eix\\Core\\ClassLoader')) {
+	$classLoader = \Nohex\Eix\Core\ClassLoader::getInstance();
+} else {
+	$classLoaderLocation = __DIR__ . '/main/Nohex/Eix/Core/ClassLoader.php';
+	if (is_readable($classLoaderLocation)) {
+		// This is the Eix library, so a direct class require is tried. Any
+		// other library should be using Eix's phar, so a require of that phar
+		// should be used here.
+		$classLoader = require_once __DIR__ . '/main/Nohex/Eix/Core/ClassLoader.php';
+	} else {
+		throw new RuntimeException('Eix is not available.');
+	}
+}
+// Add the main source folder to the include path.
+$classLoader->addPath(__DIR__ . '/main');
+// Add the libraries folder to the include path.
+$classLoader->addPath(__DIR__ . '/lib');
+// Add the test source folder to the include path.
+$classLoader->addPath(__DIR__ . '/test');
+// Initialise Eix's class loader.
+$classLoader->init();
