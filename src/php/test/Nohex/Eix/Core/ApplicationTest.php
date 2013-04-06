2da7baee9a96720510fee08059ce18f31f05808a
Max Noé <max@nohex.com>
2013-04-06 02:34:30 +0200

    Improved application tests.

--- src/php/test/Nohex/Eix/Core/ApplicationTest.php
+++ src/php/test/Nohex/Eix/Core/ApplicationTest.php
@@ -20,21 +20,92 @@ class ApplicationTest extends \PHPUnit_Framework_TestCase
         $this->assertSame($applicationA, $applicationB);
     }
 
-    public function testRawErrorsOn() {
+    /**
+     * @expectedException PHPUnit_Framework_Error
+     */
+    public function testErrorWithRawErrorsOn()
+    {
         $settings = new MockSettings;
-        $settings->application->rawErrors = 'off';
-        $application = new Application($settings);
+        $settings->application->rawErrors = 'on';
+        $application = new MockApplication($settings);
+
+        $currentHandler = set_error_handler('handler');
+        restore_error_handler();
+        $this->assertNotNull($currentHandler, 'The error handler was not assigned.');
+
+        // The error handler should not catch this one.
+        trigger_error('Testing rawErrors');
     }
 
-    public function testRawErrorsOff() {
+    /**
+     * @expectedException Exception
+     */
+    public function testExceptionWithRawErrorsOn()
+    {
         $settings = new MockSettings;
-        $settings->application->rawErrors = 'off';
-        $application = new Application($settings);
+        $settings->application->rawErrors = 'on';
+        $application = new MockApplication($settings);
+
+        $currentHandler = set_exception_handler('handler');
+        restore_exception_handler();
+        $this->assertNull($currentHandler, 'The exception handler is assigned.');
+
+        // The error handler should not catch this one.
+        throw new Exception('Testing rawErrors');
     }
 
-    public function testNoRawErrors() {
+    /**
+     * @expectedException RuntimeException
+     */
+    public function testErrorWithRawErrorsOff()
+    {
+        $settings = new MockSettings;
+        $settings->application->rawErrors = 'off';
+        $application = new MockApplication($settings);
+
+        $currentHandler = set_error_handler('handler');
+        restore_error_handler();
+        $this->assertNotNull($currentHandler, 'The error handler was not assigned.');
+    }
+
+    /**
+     * @expectedException RuntimeException
+     */
+    public function testExceptionWithRawErrorsOff()
+    {
+        $settings = new MockSettings;
+        $settings->application->rawErrors = 'off';
+        $application = new MockApplication($settings);
+
+        $currentHandler = set_exception_handler('handler');
+        restore_exception_handler();
+        $this->assertNotNull($currentHandler, 'The exception handler was not assigned.');
+    }
+
+    /**
+     * This one must behave just as if the setting was off.
+     *
+     * @expectedException RuntimeException
+     */
+    public function testRawErrorsNotSet()
+    {
         $settings = new MockSettings;
         unset($settings->application->rawErrors);
-        $application = new Application($settings);
+        $application = new MockApplication($settings);
+
+        try {
+            // The setting should not exist.
+            $settings = $settings->application->rawErrors;
+        } catch (SettingsException $exception) {
+            // The exception is expected, go on.
+        }
+
+        $currentHandler = set_error_handler('handler');
+        restore_error_handler();
+        $this->assertNotNull($currentHandler, 'The error handler is assigned.');
+
+        $currentHandler = set_exception_handler('handler');
+        restore_exception_handler();
+        $this->assertNull($currentHandler, 'The exception handler is assigned.');
     }
 }
